a95a3c5d94f324d5ff1547e37f2e0ab540c64b15
asuforce <owata.sn@gmail.com>
2018-02-23 20:06:03 +0900

    fix nitpicks

--- setup.sh
+++ setup.sh
@@ -31,7 +31,7 @@ readonly DEIN_FILES=(dein.toml dein_lazy.toml)
 
 for file in ${DOT_FILES[@]}
 do
-  dest_file=$HOME/$file
+  dest_file="$HOME/$file"
   if [ -e $dest_file ]; then
     ln -fs $REPO_DIR/$file $dest_file
   fi
@@ -39,7 +39,7 @@ done
 
 for file in ${DEIN_FILES[@]}
 do
-  dest_file=$HOME/.vim/dein/$file
+  dest_file="$HOME/.vim/dein/$file"
   if [ -e $dest_file ]; then
     ln -fs $REPO_DIR/.vim/dein/$file $dest_file
   fi
@@ -64,7 +64,6 @@ fi
 if [ ! -d ~/.ssh  ]; then
   mkdir ~/.ssh/conf.d
 fi
-
 
 # install mkr
 if [ ! -e /usr/local/bin/mkr ] ; then
