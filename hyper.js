a7ac686a83b2e92bad2941e638d14d0192a24740
asuforce <owata.sn@gmail.com>
2018-09-28 15:28:58 +0900

    Set 0.70

--- hyper.js
+++ hyper.js
@@ -124,7 +124,7 @@ module.exports = {
 
     // for advanced config flags please refer to https://hyper.is/#cfg
 
-    opacity: 0.90,
+    opacity: 0.70,
   },
 
   // a list of plugins to fetch and install from npm
